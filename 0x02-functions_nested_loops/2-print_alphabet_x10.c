7652665a459db23095edfeccfdf79dbd8db41eca
erkwot <erkwotgads22@gmail.com>
2023-05-24 16:04:25 -0700

    wheew

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10(void) - Hello world
+ * print_alphabet_x10 - Hello world
  * Description: hello world print alphabets 10-times
  * Return: Always 0.
  */
@@ -10,6 +10,7 @@ void print_alphabet_x10(void)
 {
 	char ch[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	int i;
+	int j;
 
 	for (i = 0; i <= 10; i++)
 	{
