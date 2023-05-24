1371f35e19f6f72f0674f7fe1510cd8997af2215
erkwot <erkwotgads22@gmail.com>
2023-05-24 15:59:01 -0700

    whasw

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,14 @@
 
 void print_alphabet_x10(void)
 {
-	char ch='a';
+	char ch[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	int i;
 
 	for (i = 0; i <= 10; i++)
 	{
-		while (ch <= 'z')
+		for (j = 0; j <= sizeof(ch); j++)
 		{
-			_putchar(ch);
-			ch++;
+			_putchar(j);
 		}
 		_putchar('\n');
 	}
